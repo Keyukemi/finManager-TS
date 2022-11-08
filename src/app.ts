7bcecdb7ce154444db8d8e14a259c6e86ef9be09
Keyukemi <keyukemi.ku@gmail.com>
2022-11-08 20:27:58 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import {Invoice} from './classes/invoice.js';
+import {Payment} from './classes/payments.js';
+
+const invOne = new Invoice('lily', 'New website', 250)
+const invTwo = new Invoice('LOLO', 'Old website', 200)
+
+console.log(invOne, invTwo)
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) =>{
+    e.preventDefault();
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    );
+})
